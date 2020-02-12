d61826023bd4c74dbc19ede3e9f04b0aaf0cb714
Christopher Leja <christopher.leja@gmail.com>
2020-02-12 02:52:39 +0000

    Automatically backed up by Learn

--- spec/a_broken_program_spec.rb
+++ spec/a_broken_program_spec.rb
@@ -1,9 +1,9 @@
 describe 'A broken program' do
 
-  it 'raises an error when loaded' do
+  it 'not to raise an error when loaded' do
     expect{
       load 'lib/a_broken_program.rb'
-    }.to raise_error
+    }.notto raise_error
   end
 
 end
